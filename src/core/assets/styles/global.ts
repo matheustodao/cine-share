9a3be6d3697556a867cb311ea6d22b4b14b76b4b
matheustodao <todaomatheus@hotmail.com>
2023-05-06 17:47:14 -0300

    refactor: change static values into theme
    feat:  styling `body` tag

--- src/core/assets/styles/global.ts
+++ src/core/assets/styles/global.ts
@@ -1,12 +1,17 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, css } from 'styled-components';
 
-export const GlobalStyle = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle(({ theme }) => css`
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     outline: none;
 
-    font-size: 62.5%;
+    font-size: ${theme.fonts.sizePoints};
   }
-`;
+
+  body {
+    background: ${theme.colors.bg.app};
+    margin: 1.6rem
+  }
+`);
