d04248dcfd86d042e0e6fd06103f6cc6fa05a9f2
matheustodao <todaomatheus@hotmail.com>
2023-05-06 19:48:41 -0300

    feat:  create new theme types

--- src/domain/styled-components.d.ts
+++ src/domain/styled-components.d.ts
@@ -3,6 +3,11 @@ import 'styled-components';
 
 type DefaultThemeType = typeof DEFAULT_THEME;
 
+export type TitleFontWeightType = keyof DefaultThemeType['fonts']['title']['weight'];
+export type BodyFontWeightType = keyof DefaultThemeType['fonts']['body']['weight'];
+export type SizesFontType = keyof DefaultThemeType['fonts']['sizes'];
+export type TextColorType = keyof DefaultThemeType['colors']['text'];
+
 declare module 'styled-components' {
   export interface DefaultTheme extends DefaultThemeType { }
 }
