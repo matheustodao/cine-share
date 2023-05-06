abd503f04170f3f5a192354dcfcb4d517ee92aaf
matheustodao <todaomatheus@hotmail.com>
2023-05-06 17:48:17 -0300

    feat: add GlobalStyle

--- src/core/layouts/provider/index.tsx
+++ src/core/layouts/provider/index.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from 'styled-components';
 
+import { GlobalStyle } from 'core/assets/styles/global';
 import { DEFAULT_THEME } from 'core/assets/styles/themes';
 import { IChildren } from 'domain/core/core';
 
 export function Provider({ children }: IChildren) {
   return (
     <ThemeProvider theme={DEFAULT_THEME}>
+      <GlobalStyle />
       {children}
     </ThemeProvider>
   );
