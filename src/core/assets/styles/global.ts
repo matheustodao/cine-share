8ec95e5f3447f3c59a866d818e73707ae22566fe
matheustodao <todaomatheus@hotmail.com>
2023-05-07 01:12:14 -0300

    feat: styling scrollbar

--- src/core/assets/styles/global.ts
+++ src/core/assets/styles/global.ts
@@ -7,6 +7,31 @@ export const GlobalStyle = createGlobalStyle(({ theme }) => css`
     box-sizing: border-box;
     outline: none;
     border: none;
+
+    ::-webkit-scrollbar {
+      width: 0.2rem;
+    }
+
+    ::-webkit-scrollbar:horizontal {
+      height: 0.2rem;
+    }
+
+    ::-webkit-scrollbar-track {
+      background-color: rgba(0, 0, 0, 0);
+    }
+
+    ::-moz-scrollbar-track {
+      background-color: rgba(0, 0, 0, 0);
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background: ${theme.colors.brand[800]};
+      width: 2px;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+      background: ${theme.colors.brand[300]};
+    }
   }
 
   :root {
