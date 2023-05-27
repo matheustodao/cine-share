387bc83d1f0892255794063e9c5769e525664df8
matheustodao <todaomatheus@hotmail.com>
2023-05-27 14:37:20 -0300

    feat: add `CineCardLoader` component

---
+++ src/presentation/components/CineCard/loader/index.tsx
@@ -0,0 +1,20 @@
+import Skeleton from 'react-loading-skeleton';
+import { ThemeConsumer } from 'styled-components';
+import { CineCardLoaderProps } from 'types/presentation/components/cine';
+
+export function CineCardLoader({ count, wrapper }: CineCardLoaderProps) {
+  return (
+    <ThemeConsumer>
+      {({ colors, rounded }) => (
+        <Skeleton
+          width={130}
+          height={205}
+          borderRadius={rounded.small}
+          count={count}
+          wrapper={wrapper}
+          baseColor={colors.blackAlpha[50]}
+        />
+      )}
+    </ThemeConsumer>
+  );
+}
