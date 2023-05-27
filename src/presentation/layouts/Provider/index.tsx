7f56bc92f01ac55f0d14ff7f77fa28226aae09d2
matheustodao <todaomatheus@hotmail.com>
2023-05-27 14:36:55 -0300

    feat: add global import `react-loading-skeleton`

--- src/presentation/layouts/Provider/index.tsx
+++ src/presentation/layouts/Provider/index.tsx
@@ -4,6 +4,7 @@ import { GlobalStyle } from 'presentation/assets/styles/global';
 import { DEFAULT_THEME } from 'presentation/assets/styles/themes';
 import { IChildren } from 'types/presentation/core';
 
+import 'react-loading-skeleton/dist/skeleton.css';
 import 'swiper/css';
 import 'swiper/css/mousewheel';
 import 'swiper/css/navigation';
