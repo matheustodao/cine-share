7966349ef146b9a49de4ef3b2733685fd05a2167
matheustodao <todaomatheus@hotmail.com>
2023-05-27 14:16:35 -0300

    feat(swiper): add globally imports to use `swiper`

--- src/presentation/layouts/Provider/index.tsx
+++ src/presentation/layouts/Provider/index.tsx
@@ -4,6 +4,10 @@ import { GlobalStyle } from 'presentation/assets/styles/global';
 import { DEFAULT_THEME } from 'presentation/assets/styles/themes';
 import { IChildren } from 'types/presentation/core';
 
+import 'swiper/css';
+import 'swiper/css/mousewheel';
+import 'swiper/css/navigation';
+
 export function Provider({ children }: IChildren) {
   return (
     <ThemeProvider theme={DEFAULT_THEME}>
