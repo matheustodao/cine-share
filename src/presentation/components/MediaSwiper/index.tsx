2815ec3e5d4bd49be4f8fdc7f03e22a0c7f1086d
matheustodao <todaomatheus@hotmail.com>
2023-05-28 05:08:07 -0300

    refactor: change styles swiper
    refactor: disable mousewheel in swiper
    
    refactor: add key at elements in component loader
    
    refactor: change link to button navigation

--- src/presentation/components/MediaSwiper/index.tsx
+++ src/presentation/components/MediaSwiper/index.tsx
@@ -1,5 +1,6 @@
+import { useRouter } from 'next/navigation';
+
 import { CaretRight } from '@phosphor-icons/react';
-import Link from 'next/link';
 import { SwiperSlide } from 'swiper/react';
 
 import { CineCard } from 'presentation/components/CineCard';
@@ -14,29 +15,39 @@ import { Container } from './styles';
 export function MediaSwiper({
   title, link, media, loading,
 }: MediaSwiperProps) {
+  const router = useRouter();
+
+  function handleRedirect() {
+    router.push(link);
+  }
+
   if (loading) {
     return <MediaSwiperLoader />;
   }
 
-  if (media.length === 0) {
+  if (media?.length === 0) {
     return null;
   }
 
   return (
     <Container>
       <header>
-        <Link href={link}>
-          <Title as="small" schema={600} weight="medium">
+        <button type="button" onClick={handleRedirect} tabIndex={0}>
+          <Title as="small" schema={500} weight="medium">
             {title}
             <CaretRight size={24} />
           </Title>
-        </Link>
+        </button>
       </header>
 
       <MediaSwiperList>
-        {media.map((item) => (
+        {media?.map((item) => (
           <SwiperSlide key={item.id}>
-            <CineCard {...item} />
+            <CineCard
+              id={item.id}
+              image={item.poster_path}
+              title={item.title}
+            />
           </SwiperSlide>
         ))}
       </MediaSwiperList>
