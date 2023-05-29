f45eb42531c8bb77993a13a7dd42057364952289
matheustodao <todaomatheus@hotmail.com>
2023-05-29 19:10:53 -0300

    refactor: mock data return

--- src/server/modules/media/getMovies/index.ts
+++ src/server/modules/media/getMovies/index.ts
@@ -1,8 +1,13 @@
-import { tmdbApi } from 'server/api/tmdb';
+// import { tmdbApi } from 'server/api/tmdb';
 import { DiscoverMovieQueryParams } from 'types/server/tmdb';
 
+import mockMedia from 'mock/media.json';
+
 export async function getMoviesUseCase(options?: DiscoverMovieQueryParams) {
-  return tmdbApi.get('/discover/movie', {
-    params: options,
-  });
+  // return tmdbApi.get('/discover/movie', {
+  //   params: options,
+  // });
+  console.log({ movie: options });
+
+  return { data: mockMedia };
 }
--- src/server/modules/media/getTvShows/index.ts
+++ src/server/modules/media/getTvShows/index.ts
@@ -1,10 +1,16 @@
-import { tmdbApi } from 'server/api/tmdb';
 import { DiscoverTVQueryParams } from 'types/server/tmdb';
 
+import mockMedia from 'mock/media.json';
+
 export async function getTvShowsUseCase(options?: DiscoverTVQueryParams) {
-  return tmdbApi.get('/discover/tv', {
-    params: {
-      ...options,
-    },
-  });
+  // return tmdbApi.get('/discover/tv', {
+  //   params: {
+  //     ...options,
+  //   },
+  // });
+  console.log({ tv: options });
+
+  return {
+    data: mockMedia,
+  };
 }
