787474c7c29e23910130a577b969c8d1367eab74
matheustodao <todaomatheus@hotmail.com>
2023-06-03 19:01:49 -0300

    style: remove console

--- src/infra/services/media/getMovies/index.ts
+++ src/infra/services/media/getMovies/index.ts
@@ -2,7 +2,6 @@ import { cineShareApi } from 'infra/api/cineShareApi';
 import { DiscoverMovieQueryParams } from 'types/server/tmdb';
 
 export async function getMovies(options?: DiscoverMovieQueryParams) {
-  console.log(options);
   return (await cineShareApi.get('/media/movies', {
     params: options,
   })).data;
