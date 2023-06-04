c54cc192d8006e0fb97f3168f15a6fddbaf8b03a
matheustodao <todaomatheus@hotmail.com>
2023-06-04 04:08:25 -0300

    refactor: add max name length

--- src/presentation/validations/collection/create.ts
+++ src/presentation/validations/collection/create.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 import { zodLocales } from '../locales';
 
 export const validationSchemaCreateCollection = z.object({
-  name: z.string().nonempty(zodLocales.required),
+  name: z.string().max(26, { message: 'Deve conter no máximo 26 caracteres' }).nonempty(zodLocales.required),
   description: z.string().max(440, { message: 'Deve conter no máximo 440 caracteres' }),
 });
 
