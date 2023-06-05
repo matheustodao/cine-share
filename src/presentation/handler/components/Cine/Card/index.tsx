f0c3af98f5c6893ba6aa47b7d594d8ee7fcd4dfa
matheustodao <todaomatheus@hotmail.com>
2023-06-04 22:55:22 -0300

    refactor: add validation if user is authenticated

--- src/presentation/handler/components/Cine/Card/index.tsx
+++ src/presentation/handler/components/Cine/Card/index.tsx
@@ -1,9 +1,20 @@
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { toast } from 'react-toastify';
 
 export function useCineCardHandler() {
   const [addMediaCollectionModal, setAddMediaCollectionModal] = useState<boolean>(false);
+  const { status } = useSession();
+  const router = useRouter();
 
   function handleOpenAddMediaCollectionModal() {
+    if (status === 'unauthenticated') {
+      toast.info('Realiza o login e começa compartilhar suas recomendações com seus amigos!', {
+        position: 'top-center',
+      });
+      return router.push('/login');
+    }
     setAddMediaCollectionModal(true);
   }
 
