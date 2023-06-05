654cd31f885aa187eb7dc094b7824cbcd2db17ff
matheustodao <todaomatheus@hotmail.com>
2023-06-04 21:56:50 -0300

    fix: baseURL cineshare api

--- src/infra/api/cineShareApi.ts
+++ src/infra/api/cineShareApi.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
-import { NEXT_PUBLIC_NEXT_URL } from 'infra/config/env';
 
 export const cineShareApi = axios.create({
-  baseURL: `${NEXT_PUBLIC_NEXT_URL}/api`,
+  baseURL: '/api',
 });
