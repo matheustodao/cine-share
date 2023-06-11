3512c672f001d22130831a7bee267dedf0428577
matheustodao <todaomatheus@hotmail.com>
2023-06-11 15:14:02 -0300

    fix: ILocaleResource generic type

--- src/types/i18n.d.ts
+++ src/types/i18n.d.ts
@@ -3,19 +3,22 @@ import 'i18next';
 import 'react-i18next';
 
 import Common from 'app/i18n/locales/pt-BR/common.json';
+import { Paths } from 'types';
 
 interface IResources {
   common: typeof Common
 }
 
-type ILocaleResourcesType<ResourceType extends keyof IResources | undefined = undefined> = ResourceType extends keyof IResources
-  ? TPaths<IResources[ResourceType]>
-  : IResources extends undefined ? TPaths<IResources> : TPaths<IResources>;
+type IResourcesKeys = keyof IResources;
+
+export type ILocaleResourcesType<ResourceType extends IResourcesKeys | undefined = undefined> = ResourceType extends IResourcesKeys
+  ? Paths<IResources[ResourceType]>
+  : IResources extends undefined ? Paths<IResources> : Paths<IResources>;
 
 declare module 'i18next' {
   interface CustomTypeOptions {
     resources: IResources;
   }
 
-  export type ILocaleResources<T extends keyof IResource | undefined = undefined> = ILocaleResourcesType<T>;
+  export type ILocaleResources<T extends IResourcesKeys | undefined = undefined> = ILocaleResourcesType<T>;
 }
